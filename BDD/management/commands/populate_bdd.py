7a21834f5fbb38afba70272c9bd36d30191ea5e0
Wanysteus <20403813+wanysteus@users.noreply.github.com>
2018-03-28 17:39:52 +0200

    add file deletion

--- BDD/management/commands/populate_bdd.py
+++ BDD/management/commands/populate_bdd.py
@@ -1,61 +1,116 @@
 from django.core.management.base import BaseCommand
 from BDD.models import Beamy, BeamyUser
+from storage.models import FileImage, FileSong, FileVideo, FileUser
 from django.contrib.auth.models import User
+import os
+from back.settings.base import BASE_DIR
+from django.core.files import File
+
+root = os.path.join(BASE_DIR, "../../testfiles/")
+
 
 class Command(BaseCommand):
     args = '<foo bar ...>'
     help = 'our help string comes here'
 
     def _create_beamy(self):
-        try :
-            Beamy.objects.get(name = 'death_star')
+        try:
+            Beamy.objects.get(name='death_star')
 
         except:
-            bdeath_star = Beamy(name = 'death_star', id_version = '1.0', pin = 1234)
+            bdeath_star = Beamy(name='death_star', id_version='1.0', pin=1234)
             bdeath_star.save()
 
-        try:            
-            Beamy.objects.get(name = 'falcon')
+        try:
+            Beamy.objects.get(name='falcon')
 
         except:
-            bfalcon = Beamy(name = 'falcon', id_version = '1.0', pin = 4321)
+            bfalcon = Beamy(name='falcon', id_version='1.0', pin=4321)
             bfalcon.save()
-    
+
     def _create_user(self):
-        try :
-            User.objects.get(username = 'vador')
+        try:
+            User.objects.get(username='vador')
 
         except:
-            uvador = User(username = 'vador', password = "azerty")
+            uvador = User(username='vador', password="azerty")
             uvador.save()
-        
-        try :
-            User.objects.get(username = 'solo')
+
+        try:
+            User.objects.get(username='solo')
 
         except:
-            usolo = User(username = 'solo', password = "azerty")
+            usolo = User(username='solo', password="azerty")
             usolo.save()
-    
+
     def _create_beamy_user(self):
-        try :
-            BeamyUser.objects.get(id_beamy__name = 'death_star', id_user__username = 'vador')
-            
-        except:
-            vador = User.objects.get(username = 'vador')
-            death_star = Beamy.objects.get(name = 'death_star')
-            budv = BeamyUser(id_beamy = death_star, id_user = vador, right = 'owner')
-            budv.save()
-        
-        try :
-            BeamyUser.objects.get(id_beamy__name = 'falcon', id_user__username = 'solo')
+        try:
+            BeamyUser.objects.get(
+                id_beamy__name='death_star', id_user__username='vador')
 
         except:
-            solo = User.objects.get(username = 'solo')
-            falcon = Beamy.objects.get(name = 'falcon')
-            bufs = BeamyUser(id_beamy = falcon, id_user = solo, right = 'owner')
+            vador = User.objects.get(username='vador')
+            death_star = Beamy.objects.get(name='death_star')
+            budv = BeamyUser(id_beamy=death_star, id_user=vador, right='owner')
+            budv.save()
+
+        try:
+            BeamyUser.objects.get(id_beamy__name='falcon',
+                                  id_user__username='solo')
+
+        except:
+            solo = User.objects.get(username='solo')
+            falcon = Beamy.objects.get(name='falcon')
+            bufs = BeamyUser(id_beamy=falcon, id_user=solo, right='owner')
             bufs.save()
-    
+
+    def _create_image(self):
+        try:
+            FileImage.objects.get(name="image1")
+
+        except:
+            path = os.path.join(root, "image1.jpg")
+            image = FileImage(name='image1', image=File(open(path, 'rb')))
+            image.save()
+
+        try:
+            FileImage.objects.get(name="image2")
+
+        except:
+            path = os.path.join(root, "image2.jpg")
+            image = FileImage(name='image2', image=File(open(path, 'rb')))
+            image.save()
+
+    def _create_video(self):
+        try:
+            FileVideo.objects.get(name="video1")
+
+        except:
+            path = os.path.join(root, "video1.mp4")
+            video = FileVideo(name='video1', video=File(open(path, 'rb')))
+            video.save()
+
+    def _create_song(self):
+        try:
+            FileSong.objects.get(name="song1")
+
+        except:
+            path = os.path.join(root, "song1.mp3")
+            song = FileSong(name='song1', song=File(open(path, 'rb')))
+            song.save()
+
+        try:
+            FileSong.objects.get(name="song2")
+
+        except:
+            path = os.path.join(root, "song2.mp3")
+            song = FileSong(name='song2', song=File(open(path, 'rb')))
+            song.save()
+
     def handle(self, *args, **options):
         self._create_beamy()
         self._create_user()
         self._create_beamy_user()
+        self._create_image()
+        self._create_video()
+        self._create_song()
--- storage/models.py
+++ storage/models.py
@@ -4,7 +4,10 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from BDD.models import Beamy
 from django.core.exceptions import ValidationError
-from datetime import datetime
+from datetime import datetime, timedelta
+from django.dispatch import receiver
+import uuid
+import os
 
 # Create your models here.
 
@@ -23,26 +26,32 @@ class FileUser(models.Model):
 
 
 class FileImage(models.Model):
+	def upload_path(self, filename):
+		return "images/{}-{}.{}".format(self.name, uuid.uuid4(), filename.split(".")[-1])
+
 	name = models.CharField(max_length = 42)
 	filesize = models.PositiveIntegerField(default=0)
 	resolution = models.PositiveIntegerField(default=0)
 	date = models.DateTimeField(default=now)
 	GPS = models.CharField(max_length = 42, blank=True)
 	form = models.CharField(max_length = 42, blank=True)
-	image = models.ImageField(upload_to="images/")
+	image = models.ImageField(upload_to=upload_path)
 
 	class Meta:
 		db_table = 'storage_fileimage'
 
 
 class FileSong(models.Model):
+	def upload_path(self, filename):
+		return "song/{}-{}.{}".format(self.name, uuid.uuid4(), filename.split(".")[-1])
+
 	name = models.CharField(max_length = 42)
 	filesize = models.IntegerField(default = 0)
 	resolution = models.IntegerField(default = 0)
-	date = models.DateTimeField(default =now)
+	date = models.DateTimeField(default = now)
 	form = models.CharField(max_length = 42, blank=True)
-	length = models.DurationField(default=0)
-	song = models.FileField(upload_to="songs/")
+	length = models.DurationField(default=timedelta())
+	song = models.FileField(upload_to=upload_path)
 
 	def clean(self):
 		accepted_format = ['mp3', 'wma']
@@ -59,15 +68,18 @@ class FileSong(models.Model):
 
 
 class FileVideo(models.Model):
+	def upload_path(self, filename):
+		return "video/{}-{}.{}".format(self.name, uuid.uuid4(), filename.split(".")[-1])
+
 	name = models.CharField(max_length = 42)
 	filesize = models.PositiveIntegerField(default=0)
 	resolution = models.PositiveIntegerField(default=0)
 	date = models.DateTimeField(default=now)
 	form = models.CharField(max_length = 42, blank=True)
-	length = models.DurationField(default=0)
+	length = models.DurationField(default=timedelta())
 	album = models.CharField(max_length=42, blank=True)
 	artist = models.CharField(max_length=42, blank=True)
-	video = models.FileField(upload_to="videos/")
+	video = models.FileField(upload_to=upload_path)
 
 	def clean(self):
 		accepted_format = ['mp4', 'avi', 'mkv']
@@ -81,3 +93,33 @@ class FileVideo(models.Model):
 
 	class Meta:
 		db_table = 'storage_filevideo'
+
+@receiver(models.signals.post_delete, sender=FileSong)
+def auto_delete_song_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.song:
+        if os.path.isfile(instance.song.path):
+            os.remove(instance.song.path)
+
+@receiver(models.signals.post_delete, sender=FileImage)
+def auto_delete_image_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+
+@receiver(models.signals.post_delete, sender=FileVideo)
+def auto_delete_video_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.video:
+        if os.path.isfile(instance.video.path):
+            os.remove(instance.video.path)
