e947673a7bb1e8e2fa07c80f704bc4e2af165669
Arthur <cauchetier.arthur@laposte.net>
2018-03-28 17:49:39 +0200

    add url storage

--- storage/urls.py
+++ storage/urls.py
@@ -13,18 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/alarm/', include('alarm.urls')),
-    path('api/files/', include('storage.urls')),
+    path('image/', views.fileImage, name = 'fileImage'),
 ]
-
-# Be careful when deployding to production
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
