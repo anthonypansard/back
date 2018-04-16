09aa01cfbebf1980f09f393efe92834b31a4a6c6
Wanysteus <20403813+wanysteus@users.noreply.github.com>
2018-04-16 20:43:13 +0200

    comment alarm code

--- alarm/models.py
+++ alarm/models.py
@@ -7,27 +7,36 @@ from django.core.exceptions import ValidationError
 class Alarm(models.Model):
 	"""
 	Class to save alarm information in database.
-	More work needs to be done with timezone management. Maybe link each beamy to it's timezone and display the right timezone in the app ?
+	Time zones are not managed. We assume that the date/hour saved in the model is the one the beamy is set on.
 	"""
-	name		= models.CharField(default = 'alarm', max_length = 42)
-	enabled 	= models.CharField(default = 'true', max_length = 42)
-	running 	= models.CharField(default = 'false', max_length = 42)
-	day			= models.CharField(default = 'monday', max_length = 100)
-	hour		= models.IntegerField(default = 0)
-	minute		= models.IntegerField(default = 0)
-	beamy		= models.ForeignKey(Beamy, on_delete = models.CASCADE, default = 1)
-	# lack of id_music
+	name	= models.CharField(default = 'alarm', max_length = 42)
+	enabled	= models.CharField(default = 'true', max_length = 42)
+	running = models.CharField(default = 'false', max_length = 42)
+	day		= models.CharField(default = 'monday', max_length = 100)
+	hour	= models.IntegerField(default = 0)
+	minute	= models.IntegerField(default = 0)
+	beamy	= models.ForeignKey(Beamy, on_delete = models.CASCADE, default = 1)
+	# The possibility to choose the alarm's music (a song the client has uploaded before in the storage space) is not yet implemented
+
+	# This function is called before saving the Alarm object in the database
+	# It's purpose is to check that the data is correct
+	# We don't wan't corrupted data to be stored and cause crashes afterwards
 	def clean(self):
+		# The day string may contain spaces. We delete them
 		self.day = self.day.replace(" ", "")
+		# We check if days in day are existing days of the week
 		if not set(self.day.split(",")).issubset(["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]):
 			raise ValidationError('Bad data : "day" value should be elements of "[monday, tuesday, wednesday, thursday, friday, saturday, sunday]" separated by commas')
+		# The time is represented using the 24-hour format
 		if self.hour < 0 or self.hour > 23:
 			raise ValidationError('Bad data : "hour" value should be between 0 and 23')
 		if self.minute < 0 or self.minute > 59:
 			raise ValidationError('Bad data : "minute" value should be between 0 and 59')
+		# The Alarm can either be enabled -> 'true' or disabled -> 'false'
 		if self.enabled not in ('true', 'false'):
 			raise ValidationError('Bad data : "enabled" value should be "true" or "false"')
-		
+	
+	# This overides the save method and call clean() before saving any Alarm object
 	def save(self, **kwargs):
 		self.clean()
 		return super(Alarm, self).save(**kwargs)
@@ -35,5 +44,6 @@ class Alarm(models.Model):
 	class Meta:
 		db_table = 'alarm_alarm'
 
+	# This defines what is the Alarm's name in Django's administration interface
 	def __str__(self):
 		return self.name
--- alarm/views.py
+++ alarm/views.py
@@ -9,8 +9,17 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def buildAlarmResponse(alarmList):
+	"""
+	Create the xml formated text that will be sent back to the request's sender
+
+	@param :	alarmList, a list of Alarm objects
+	@return: 	content  , a string containing the xml formated text (pretty printed) 
+						   with all relevant data about the requested Alarm objects 
+	"""
+	# This is not mandatory
 	content = "<?xml version=\"1.0\"?>\n"
 
+	# The xml text holds data like a tree
 	root = etree.Element("set")
 	for alarm in alarmList:
 		beamy = alarm.beamy
@@ -39,7 +48,25 @@ def buildAlarmResponse(alarmList):
 
 @csrf_exempt
 def detail(request, alarm_id):
+	"""
+	Return all relevant data about a particular Alarm object, designated by it's id (int) in a xml formated text file
+	or
+	Detele a particular Alarm object, designated by it's id (int) and return
+
+	@param :	request     , a HTTP request (GET or DELETE)
+				alarm_id    , an integer which must correspond to the id of an existing Alarm
+	@return:	HttpResponse, (GET) a file containing the xml formated text (pretty printed) 
+							  with all relevant data about the requested Alarm object
+							  or
+							  (DETELE) HTTP status code 200 alone if the DELETE request was succesful
+	@errors:	400			, the request does not have the required format :
+								- it might no be a DELETE or GET request
+								- the url requested might not be exactly the same as defined in ./urls.py
+				404			, alarm_id does not match any existing Alarm object
+	"""
 	try:
+		# We try to get the Alarm object which id is 'alarm_id'
+		# raises DoesNotExist Exception when not found
 		alarm = Alarm.objects.get(pk=alarm_id)
 
 		if request.method == 'GET':
@@ -48,36 +75,59 @@ def detail(request, alarm_id):
 		
 		elif request.method == 'DELETE':
 			alarm.delete()
-			return HttpResponse("hello")
+			return HttpResponse(status = 200)
 		
 		else:
 			return HttpResponse(status = 400)
 
 	except Alarm.DoesNotExist:
-		raise Http404("Alarm does not exist")
+		raise Http404("The requested alarm does not exist")
 
 @csrf_exempt
 def alarm(request):
+	"""
+	Return all relevant data about all Alarm objects in a xml formated text file
+	or
+	Add an Alarm object to the database
+
+	@param :	request     , a HTTP request (GET or POST)
+	@return:	HttpResponse, (GET) a file containing the xml formated text (pretty printed) 
+							  with all relevant data about all Alarm objects
+							  or
+							  (POST) a file containing the xml formated text (pretty printed)
+							  with all relevant data about the Alarm object newly created
+	@errors:	400			, the request does not have the required format :
+								- it might no be a POST or GET request
+								- the url requested might not be exactly the same as defined in ./urls.py
+				422			, the data did not contain all the mandatory fields some fields was invalid
+				404			, alarm_id does not match any existing Alarm object
+	"""
 	if request.method == 'GET':
 		alarmList = Alarm.objects.order_by('id')
+		# If there is no Alarm in the database,
+		# we send back a xml file with an empty '<set>' mark
 		if not alarmList:
-			return HttpResponse("Empty list", status = 500)
-		content = buildAlarmResponse(alarmList)
+			content = "<?xml version=\"1.0\"?>\n<set>\n</set>"
+		else:
+			content = buildAlarmResponse(alarmList)
 		return HttpResponse(content, content_type='text/xml')
 
 	elif request.method == 'POST':
+		# Only one Alarm object can be created for each request
+		# If more than one alarm need to be created, one must make many POST requests
 		try :
+			# The following fields are mandatory
+			# An IndexError is raised if a field is missing
 			req = request.read().decode("utf-8")
-			# print(req)
 			tree = etree.parse(StringIO(req))
-			# TODO : think about when multiple alarms are present in the POST request
 			name     = tree.xpath("/alarm/name")[0].text
 			day   	 = tree.xpath("/alarm/time/day")[0].text
 			hour 	 = int(tree.xpath("/alarm/time/hour")[0].text)
 			minute 	 = int(tree.xpath("/alarm/time/minute")[0].text)
 			enabled	 = tree.xpath("/alarm/enabled")[0].text
 			beamy_id = int(tree.xpath("/alarm/beamy_id")[0].text)
-		
+
+			# The beamy object must exist
 			beamy = Beamy.objects.get(pk = beamy_id)
 			
 			alarm = Alarm(
@@ -107,13 +157,26 @@ def alarm(request):
 
 @csrf_exempt
 def toggleAlarm(request, alarm_id, toggle):
+	"""
+	Change the running state of an Alarm object
+	When a Beamy starts ringing it's alarm, it must update the 'running' fiedl
+	Then any smartphone linked with the Beamy could stop the alarm
+
+	@param :	request     , a HTTP request (GET)
+				alarm_id    , an integer which must correspond to the id of an existing Alarm
+				toggle      , a string ('start' or 'stop')
+	@return:	HttpResponse, a file containing the xml formated text (pretty printed) 
+							  with all relevant data about the updated Alarm object
+	@errors:	400			, either the toggle string or the url requested is invalid
+				404			, the requested alarm does not exist
+	"""
 	if request.method == 'GET':
 		try:
 			alarm = Alarm.objects.get(pk=alarm_id)
-
+			# The 'start' toggle must be sent by the beamy
 			if toggle == 'start':
 				alarm.running = 'true'
-			
+			# The 'stop' flag can be sent by the beamy or the smartphone
 			elif toggle == 'stop':
 				alarm.running = 'false'
 			
